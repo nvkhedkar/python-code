cbca194aadc3cd1871ef77538aefe40d571e8882
Khedkar <nkhedkar@ptc.com>
2021-01-01 10:02:07 +0530

    nvk - try stuff

--- tests/try_stuff.py
+++ tests/try_stuff.py
@@ -48,52 +48,63 @@ def are_two_equal_sz3(iarr):
     return 0
 
 
-all_nums = dict()
-equals, all_eq, th_eq, two_eq = 0, 0, 0, 0
-n1 = 9
-n2 = 3
-bign = 100000
-for i in range(bign):
-    arr = [random.randint(0, n1), random.randint(0, n1), random.randint(0, n1), random.randint(0, n1)]
+def an_approxim():
+    all_nums = dict()
+    equals, all_eq, th_eq, two_eq = 0, 0, 0, 0
+    n1 = 9
+    n2 = 3
+    bign = 100000
+    for i in range(bign):
+        arr = [random.randint(0, n1), random.randint(0, n1), random.randint(0, n1), random.randint(0, n1)]
 
-    if arr[0] == arr[1] == arr[2] == arr[3]:
-        arr2 = [random.randint(0, 3), random.randint(0, 3), random.randint(0, 3), random.randint(0, 3)]
-        if arr2[0] == arr2[1] == arr2[2] == arr2[3]:
-            all_eq += 1
-        elif are_three_equal(arr2):
-            th_eq += 1
-        elif are_two_equal(arr2):
-            two_eq += 1
+        if arr[0] == arr[1] == arr[2] == arr[3]:
+            arr2 = [random.randint(0, 3), random.randint(0, 3), random.randint(0, 3), random.randint(0, 3)]
+            if arr2[0] == arr2[1] == arr2[2] == arr2[3]:
+                all_eq += 1
+            elif are_three_equal(arr2):
+                th_eq += 1
+            elif are_two_equal(arr2):
+                two_eq += 1
 
-    elif arr[0] == arr[1] == arr[2] or arr[1] == arr[2] == arr[3] \
-            or arr[2] == arr[3] == arr[0] or arr[2] == arr[3] == arr[0]:
-        arr2 = [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)]
-        if arr2[0] == arr2[1] == arr2[2]:
-            th_eq += 1
-        elif are_two_equal_sz3(arr2):
-            two_eq += 1
+        elif arr[0] == arr[1] == arr[2] or arr[1] == arr[2] == arr[3] \
+                or arr[2] == arr[3] == arr[0] or arr[2] == arr[3] == arr[0]:
+            arr2 = [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)]
+            if arr2[0] == arr2[1] == arr2[2]:
+                th_eq += 1
+            elif are_two_equal_sz3(arr2):
+                two_eq += 1
 
-    elif ((arr[0] == arr[1])
-            or (arr[0] == arr[2])
-            or (arr[0] == arr[3])
-            or (arr[1] == arr[2])
-            or (arr[1] == arr[3])
-            or (arr[2] == arr[3])
-    ):
-        arr2 = [random.randint(0, 1), random.randint(0, 1)]
-        if arr2[0] == arr2[1]:
-            two_eq += 1
+        elif ((arr[0] == arr[1])
+                or (arr[0] == arr[2])
+                or (arr[0] == arr[3])
+                or (arr[1] == arr[2])
+                or (arr[1] == arr[3])
+                or (arr[2] == arr[3])
+        ):
+            arr2 = [random.randint(0, 1), random.randint(0, 1)]
+            if arr2[0] == arr2[1]:
+                two_eq += 1
 
-    # if ((arr[0] == arr[1] and arr2[0] == arr2[1])
-    #         or (arr[0] == arr[2] and arr2[0] == arr2[2])
-    #         or (arr[0] == arr[3] and arr2[0] == arr2[3])
-    #         or (arr[1] == arr[2] and arr2[1] == arr2[2])
-    #         or (arr[1] == arr[3] and arr2[1] == arr2[3])
-    #         or (arr[2] == arr[3] and arr2[2] == arr2[3])
-    # ):
-    #     # print(arr, arr2)
-    #     equals += 1
-# print(equals, two_eq/bign, th_eq/bign, all_eq/bign, 100*float(equals) / float(bign))
-# print(two_eq / 2, )
+        # if ((arr[0] == arr[1] and arr2[0] == arr2[1])
+        #         or (arr[0] == arr[2] and arr2[0] == arr2[2])
+        #         or (arr[0] == arr[3] and arr2[0] == arr2[3])
+        #         or (arr[1] == arr[2] and arr2[1] == arr2[2])
+        #         or (arr[1] == arr[3] and arr2[1] == arr2[3])
+        #         or (arr[2] == arr[3] and arr2[2] == arr2[3])
+        # ):
+        #     # print(arr, arr2)
+        #     equals += 1
+    # print(equals, two_eq/bign, th_eq/bign, all_eq/bign, 100*float(equals) / float(bign))
+    # print(two_eq / 2, )
 
-print(bign, two_eq/bign, 100*th_eq/bign, all_eq, (two_eq + th_eq + all_eq)/bign)
+    print(bign, two_eq/bign, 100*th_eq/bign, all_eq, (two_eq + th_eq + all_eq)/bign)
+
+
+def numpy_eval():
+    import numpy
+    numpy_dict = {a: getattr(numpy, a) for a in dir(numpy)}
+    for k, v in numpy_dict.items():
+        print(k, v)
+
+
+numpy_eval()
